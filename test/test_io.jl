725d4bf20d3049379912c9ba720d465807365838
Jared Crean <jcrean01@gmail.com>
2017-09-15 14:26:48 -0400

    add the test file

---
+++ test/test_io.jl
@@ -0,0 +1,22 @@
+
+facts("----- Testing IO -----") do
+
+  fname = "test.dat"
+  q = rand(Complex128, 2, 3, 4)
+  write_binary(fname, q)
+
+  q2 = zeros(q)
+  read_binary!(fname, q2)
+
+  @fact vecnorm(q - q2) --> roughly(0.0, atol=1e-14)
+
+  # test error conditions
+  q = zeros(Complex128, 2, 3)
+  @fact_throws read_binary!(fname, q)
+
+
+  q = zeros(Complex, 2, 2)
+  @fact_throws write_binary(fname, q)
+  @fact_throws read_binary!(fname, q)
+
+end
