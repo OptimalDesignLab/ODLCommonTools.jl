db63905a3e1fd3a246aa4de637fbf9dd135fa6ae
Jared Crean <jcrean01@gmail.com>
2018-05-26 10:38:08 -0400

    install ArrayViews manually

---
+++ deps/build.jl
@@ -0,0 +1,25 @@
+# Install dependencies
+
+# install PkgFix if not present
+if !isdir(joinpath(Pkg.dir(), "PkgFix"))
+  Pkg.clone("https://github.com/OptimalDesignLab/PkgFix.jl.git")
+end
+Pkg.checkout("PkgFix", "upgrade_0.6")
+
+
+using PkgFix  # from now on, use PkgFix instead of Pkg for everything
+
+
+
+global const ARRAYVIEWS_URL = "http://github.com/JuliaArrays/ArrayViews.jl.git"
+global const ARRAYVIEWS_VER = "master"
+
+pkg_dict = PkgFix.installed()
+
+if !haskey(pkg_dict, "ArrayViews")
+  PkgFix.add(ARRYAVIEWS_URL, branch_ish=ARRAYVIEWS_VER)
+else
+  PkgFix.checkout("ArrayViews", ARRAYVIEWS_VER)
+end
+
+
